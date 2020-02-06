c977e0cbdcd31aa1678fc0697c7a9a6841101194
Niraj Pradhan <nirajpradhan@lftechnology.com>
2020-02-06 16:39:52 +0545

    change in master 1

--- VsCodeFile.sql
+++ VsCodeFile.sql
@@ -1,1 +1,11 @@
-select * from Employee;
+select * from Employee;-- List columns in all tables whose name is like 'TableName'
+SELECT 
+    TableName = tbl.table_schema + '.' + tbl.table_name, 
+    ColumnName = col.column_name, 
+    ColumnDataType = col.data_type
+FROM INFORMATION_SCHEMA.TABLES tbl
+INNER JOIN INFORMATION_SCHEMA.COLUMNS col 
+    ON col.table_name = tbl.table_name
+    AND col.table_schema = tbl.table_schema
+
+WHERE tbl.table_type = 'base table' and tbl.table_name like '%TableName%'
